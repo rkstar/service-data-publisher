1c08cb69eadfbc54ed8d0f057663bfe7975b5331
David Fudge <rkstar@mac.com>
2015-07-19 23:08:08 -0400

    added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Automatically sanitized and publish relevant data from external services when logged in.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rkstar/service-data-publisher',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
